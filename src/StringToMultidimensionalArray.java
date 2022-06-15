6e87134a1a13220023e578a987a75f29ec6a744a
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-06-15 20:56:04 +0200

    Update StringToMultidimensionalArray.java
    v1.1.0 Updated the algorithm to clean the sentence from blank spaces by using two separated methods.

--- src/StringToMultidimensionalArray.java
+++ src/StringToMultidimensionalArray.java
@@ -1,20 +1,57 @@
+/*
+ * v1.1.0
+ */
 import java.util.Arrays;
 
 public class StringToMultidimensionalArray {
 
   public static void main(String[] args) {
-    String[][] valueArray = new String[2][args[0].length()];
+    String sentence = args[0].trim();
 
-    for (int a = 0; a < 2; a++) {
-      for (int b = 0; b < args[0].length(); b++) {
-        if (a == 0) {
-          valueArray[a][b] = "" + args[0].charAt(b);
-        } else {
-          valueArray[a][b] = "" + args[0].codePointAt(b);
-        }
+    int blanksAmount = doCountBlank(sentence);
+
+    String[][] cleanedSentenceArray = new String[3][sentence.length() - blanksAmount];
+    String[][] blanksArray = new String[3][blanksAmount];
+
+    setCleanedSentenceArray(cleanedSentenceArray, sentence);
+    setBlanksArray(blanksArray, sentence);
+
+    System.out.println(Arrays.deepToString(cleanedSentenceArray));
+    System.out.println(Arrays.deepToString(blanksArray));
+  }
+
+  private static int doCountBlank(String sentenceArg) {
+   int counter = 0;
+   for (int a = 0, b = 0; a < sentenceArg.length(); a++) {
+     boolean isBlank = (sentenceArg.charAt(a) == ' ');
+     if (isBlank) {
+       counter++;
+     }
+   }
+   return counter;
+  }
+
+  private static void setCleanedSentenceArray(String[][] cleanedSentenceArrayArg, String sentenceArg) {
+    for (int a = 0, b = 0; a < sentenceArg.length(); a++) {
+      boolean isNotBlank = (sentenceArg.charAt(a) != ' ');
+      if (isNotBlank) {
+        cleanedSentenceArrayArg[0][b] = "" + sentenceArg.charAt(a);
+        cleanedSentenceArrayArg[1][b] = "" + a;
+        cleanedSentenceArrayArg[2][b] = "" + sentenceArg.codePointAt(a);
+        b++;
       }
     }
+  }
 
-    System.out.println(Arrays.deepToString(valueArray));
+  private static void setBlanksArray(String[][] blanksArrayArg, String sentenceArg) {
+    for (int a = 0, b = 0; a < sentenceArg.length(); a++) {
+      boolean isBlank = (sentenceArg.charAt(a) == ' ');
+      if (isBlank) {
+        blanksArrayArg[0][b] = " ";
+        blanksArrayArg[1][b] = "" + a;
+        blanksArrayArg[2][b] = "" + sentenceArg.codePointAt(a);
+        b++;
+      }
+    }
   }
 }
