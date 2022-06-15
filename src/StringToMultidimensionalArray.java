678448bd6b2c56ac6fa45056f9b75004e3bd8399
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-06-15 21:00:43 +0200

    Update StringToMultidimensionalArray.java
    v1.1.1 Updated the previous version by optimizing it with a single function.

--- src/StringToMultidimensionalArray.java
+++ src/StringToMultidimensionalArray.java
@@ -1,5 +1,5 @@
 /*
- * v1.1.0
+ * v1.1.1 - Two methods in one.
  */
 import java.util.Arrays;
 
@@ -13,8 +13,7 @@ public class StringToMultidimensionalArray {
     String[][] cleanedSentenceArray = new String[3][sentence.length() - blanksAmount];
     String[][] blanksArray = new String[3][blanksAmount];
 
-    setCleanedSentenceArray(cleanedSentenceArray, sentence);
-    setBlanksArray(blanksArray, sentence);
+    setCleanedSentenceArrayAndBlanksArray(cleanedSentenceArray, blanksArray, args[0]);
 
     System.out.println(Arrays.deepToString(cleanedSentenceArray));
     System.out.println(Arrays.deepToString(blanksArray));
@@ -31,26 +30,19 @@ public class StringToMultidimensionalArray {
    return counter;
   }
 
-  private static void setCleanedSentenceArray(String[][] cleanedSentenceArrayArg, String sentenceArg) {
-    for (int a = 0, b = 0; a < sentenceArg.length(); a++) {
-      boolean isNotBlank = (sentenceArg.charAt(a) != ' ');
-      if (isNotBlank) {
-        cleanedSentenceArrayArg[0][b] = "" + sentenceArg.charAt(a);
-        cleanedSentenceArrayArg[1][b] = "" + a;
-        cleanedSentenceArrayArg[2][b] = "" + sentenceArg.codePointAt(a);
-        b++;
-      }
-    }
-  }
-
-  private static void setBlanksArray(String[][] blanksArrayArg, String sentenceArg) {
-    for (int a = 0, b = 0; a < sentenceArg.length(); a++) {
+  private static void setCleanedSentenceArrayAndBlanksArray(String[][] sentenceArrayArg, String[][] blanksArrayArg, String sentenceArg) {
+    for (int a = 0, b = 0, c = 0; a < sentenceArg.length(); a++) {
       boolean isBlank = (sentenceArg.charAt(a) == ' ');
       if (isBlank) {
         blanksArrayArg[0][b] = " ";
         blanksArrayArg[1][b] = "" + a;
         blanksArrayArg[2][b] = "" + sentenceArg.codePointAt(a);
         b++;
+      } else {
+        sentenceArrayArg[0][c] = "" + sentenceArg.charAt(a);
+        sentenceArrayArg[1][c] = "" + a;
+        sentenceArrayArg[2][c] = "" + sentenceArg.codePointAt(a);
+        c++;
       }
     }
   }
